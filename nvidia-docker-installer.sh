67883ddaa47fc306f1f6842047033a0c4aa36c80
Jonas Brunsgaard <jonas.brunsgaard@gmail.com>
2018-12-18 12:12:59 +0100

    Update nvidia-docker-installer.sh

--- nvidia-docker-installer.sh
+++ nvidia-docker-installer.sh
@@ -67,7 +67,7 @@ install_nvidia_docker2() {
   distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
   curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | tee /etc/apt/sources.list.d/nvidia-docker.list
   apt-get update
-  apt-get install -y nvidia-docker2=2.0.3+docker17.03.2-1 nvidia-container-runtime=2.0.0+docker17.03.2-1
+  apt-get install -y nvidia-docker2=2.0.3+docker18.06.1-1 nvidia-container-runtime=2.0.0+docker18.06.1-1
 }
 
 set_nvidia_container_runtime() {
